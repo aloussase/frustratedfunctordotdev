ead5db20bbddb718b851e1e63e8e24d91fd10198
Alexander Goussas <alexandergoussas.thoughtworks@latam.com>
2025-11-11 21:11:41 -0500

    feat: minor wording changes

--- src/main/java/io/github/aloussase/frustratedfunctordotdev/EducationEntry.java
+++ src/main/java/io/github/aloussase/frustratedfunctordotdev/EducationEntry.java
@@ -14,7 +14,7 @@ public record EducationEntry(
                         "Escuela Superior Politécnica del Litoral",
                         "2019 - Abandoned",
                         "Bsc. Computer Science",
-                        "I abandoned my degree just before graduating in 2025 for multiple reasons. I have mixed feeelings about formal education, stemming from my experience with it throughout my life. I might or might not finish it later."
+                        "I abandoned my degree just before graduating in 2025 for multiple reasons. I have mixed feeelings about formal education, stemming from my experience with it throughout my life."
                 )
         );
     }
